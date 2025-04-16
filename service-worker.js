18ebd49aff9069bc9b6dc27bb6aeedeb8f4098d0
armistice-229 <armistice095@gmail.com>
2025-04-16 11:46:43 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'finance-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/login.html',
+  '/login.js',
+  '/index.js'
+  // Ajoute tes autres assets (CSS, JS, etc.)
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
